284af7e15b7893036e60251cbe793128f9eb678a
Mino260806 <mino260806@gmail.com>
2024-07-27 10:52:00 +0100

    Explicitely disable Gemini safety settings

--- app/src/main/java/tn/amin/keyboard_gpt/language_model/GeminiClient.java
+++ app/src/main/java/tn/amin/keyboard_gpt/language_model/GeminiClient.java
@@ -29,10 +29,10 @@ public class GeminiClient extends LanguageModelClient {
         configBuilder.maxOutputTokens = 4096;
 
         ArrayList<SafetySetting> safetySettings = new ArrayList<>();
-//        safetySettings.add(new SafetySetting(HarmCategory.HARASSMENT, BlockThreshold.MEDIUM_AND_ABOVE));
-//        safetySettings.add(new SafetySetting(HarmCategory.HATE_SPEECH, BlockThreshold.MEDIUM_AND_ABOVE));
-//        safetySettings.add(new SafetySetting(HarmCategory.SEXUALLY_EXPLICIT, BlockThreshold.MEDIUM_AND_ABOVE));
-//        safetySettings.add(new SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.MEDIUM_AND_ABOVE));
+        safetySettings.add(new SafetySetting(HarmCategory.HARASSMENT, BlockThreshold.NONE));
+        safetySettings.add(new SafetySetting(HarmCategory.HATE_SPEECH, BlockThreshold.NONE));
+        safetySettings.add(new SafetySetting(HarmCategory.SEXUALLY_EXPLICIT, BlockThreshold.NONE));
+        safetySettings.add(new SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.NONE));
 
         GenerativeModel gm = new GenerativeModel(
                 getSubModel(),
