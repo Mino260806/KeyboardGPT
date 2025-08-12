75b54ed96e0b03671cd6dad247567d3f9735628d
mino260806 <mino260806@gmail.com>
2025-08-12 11:41:39 +0100

    change default openrouter sub model

--- app/src/main/java/tn/amin/keyboard_gpt/llm/LanguageModel.java
+++ app/src/main/java/tn/amin/keyboard_gpt/llm/LanguageModel.java
@@ -8,7 +8,7 @@ public enum LanguageModel {
     Gemini("Gemini", "gemini-2.5-flash-lite", "https://generativelanguage.googleapis.com/v1beta"),
     ChatGPT("ChatGPT", "gpt-4o-mini", "https://api.openai.com/v1"),
     Groq("Groq", "llama3-8b-8192", "https://api.groq.com/openai/v1"),
-    OpenRouter("OpenRouter", "meta-llama/llama-3.3-70b-instruct:free", "https://openrouter.ai/api/v1"),
+    OpenRouter("OpenRouter", "openai/gpt-oss-20b:free", "https://openrouter.ai/api/v1"),
     Claude("Claude", "claude-3-5-sonnet-20240620", "https://api.anthropic.com/v1"),
     ;
 
